5ec68e4e8b9220f44fbbf7c6d379c0978236bbcf
blich student <student-cyber@blich.co.il>
2022-09-18 16:34:51 +0300

    Update assignment 3 + add assignment 4, label the assignments

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+# Assignment 1:
 list = [num for num in range (1000, 10000) if ((num % 100 + num // 100)**2) == num]
 print(list)
 
+# Assignment 2:
 number = int(input("Enter a number: "))
 list_number = [int(num) for num in str(number)]
 odd = 0
@@ -11,16 +13,30 @@ for num in list_number:
         even += num
     else:
         odd += num
-
 if odd == even:
     print("True")
 else:
     print("False")
 
+# Assignment 3:
 num = int(input("enter first number: "))
-x = 1
 for i in range(1, num + 1):
     s = ''
     for j in range(i, 0, -1):
         s += str(j) + ' '
     print(s)
+
+for i in range(num-1, 0, -1):
+    s = ''
+    for j in range(i, 0, -1):
+        s += str(j) + ' '
+    print(s)
+
+# # Assignment 4:
+for num in range(0, 6):
+    for num_add in range(1, 11):
+        number = num + num_add/10
+        if number % 1 == 0:
+            print(int(number))
+        else:
+            print(number)
